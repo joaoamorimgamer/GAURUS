471f1d6be8a43ba85606d397455c05b523e5b198
joaoamorimgamer <cachorro11_nando@proton.me>
2026-03-28 16:08:37 -0300

    real (true)

--- real.c
+++ real.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
-#include <string.h>
-
-int main(int argc, char **argv)
+int main()
 {
-	if(argc > 1)
-	{
-		if(strcmp(argv[1], "--help") == 0)
-		{
-			printf("%s - do nothing, succesfully\n", argv[0]);
-			printf("better version of 'true', by GNU Coreutils\n\n");
-			printf("Usage:\n");
-			printf("--help	display this help and exit\n");
-		}
-	}
 	return 0;
 }

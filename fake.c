9c14e92eabd7b8695910e0d4203bec465d50e21c
joaoamorimgamer <cachorro11_nando@proton.me>
2026-03-28 16:12:30 -0300

    fake (false)

--- fake.c
+++ fake.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-#include <string.h>
-int main(int argc, char **argv)
-{
-	if(argc > 1)
-	{
-		if(strcmp(argv[1], "--help") == 0)
-		{
-			printf("%s - Do nothing, unsuccessfully\n\n", argv[0]);
-			printf("Usage:\n");
-			printf("--help	print this help page and exit\n");
-		}
-	}
-	return 1;
-}
+int main(void){return 1;}

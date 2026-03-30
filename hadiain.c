b912280b95e039c584cd0e35955a02756a9559d4
joaoamorimgamer <cachorro11_nando@proton.me>
2026-03-30 14:40:47 -0300

    hadiain now can print logical paths

--- hadiain.c
+++ hadiain.c
@@ -1,31 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <errno.h>
 #include <unistd.h>
+#include <string.h>
 int main(int argc, char **argv)
 {
-	if(argc == 2)
+	if(argc > 1)
 	{
 	
 		if(strcmp(argv[1], "--help") == 0)
 		{
-			printf("%s: %s\n", argv[0], argv[0]);
-			printf("   Print the name of the current working directory.\n");
-			printf("   Options:\n");
-			printf("     --help print this help page and exit\n\n");
-			printf("   Exit Status:\n");
-			printf("   Returns 0 unless the current directory cannot be read.\n");
+			printf("Usage: %s [OPTION]...\n", argv[0]);
+      printf("   -L, --logical  print the logical path, containing symbolic links\n");
+      printf("   -P, --physical print the physical path, resolving all symbolic links to their real location\n");
+			printf("   --help print this help page and exit\n\n");
+      printf("If no argument is specified, default to -P.\n");
 			return 0;
 		}
+    else if(strcmp(argv[1], "--physical") == 0 || strcmp(argv[1], "-P") == 0)
+    {
+      char *cwd = getcwd(NULL, 0);
+      if(cwd != NULL)
+	    {
+		    printf("%s\n", cwd);
+        free(cwd);
+        return 0;
+	    } 
+    }
+    else if(strcmp(argv[1], "--logical") == 0 || strcmp(argv[1], "-L") == 0)
+    {
+      char *lcwd = getenv("PWD");
+      if(lcwd != NULL)
+      {
+        printf("%s\n", lcwd);
+        return 0;
+      }
+      else {
+        printf("%s: %s\n", argv[0], strerror(errno));
+        return 1;
+      }
+    }
 	}
-	/* This program name is strange because it stands for wHAt DIrectory Am i IN, don't judge me for bad names :) */
-	
-	char *cwd = malloc(sizeof(char));
-	cwd = getcwd(NULL, 0);
-	if(cwd != NULL)
-	{
-		printf("%s\n", cwd);
-	}
-	free(cwd);
-	return 0;
+  else if(argc == 1)
+  {
+    char *cwd = getcwd(NULL, 0);
+    if(cwd != NULL)
+    {
+      printf("%s\n", cwd);
+      free(cwd);
+      return 0;
+    }
+    else {
+      printf("%s: %s\n", argv[0], strerror(errno));
+      return 1;
+    }
+  }
+  return 0;
 }

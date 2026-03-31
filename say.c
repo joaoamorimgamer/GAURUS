cfdcb3b44df7f11a40b55a82b6b07a9ae7b8b050
joaoamorimgamer <cachorro11_nando@proton.me>
2026-03-31 15:55:21 -0300

    say now has -n option

--- say.c
+++ say.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 int main(int argc, char **argv)
 {
-	for(int i = 1; i < argc; i++)
-	{
-		printf("%s", argv[i]);
-		if(i < argc - 1)
-		{
-			printf(" ");
-		}
-	}
-	return 0;
+  int nline = 0;
+  int firstarg = 1;
+  if(argc > 1 && strcmp(argv[1], "-n") == 0)
+  {
+    nline = 1;
+    firstarg = 2;
+  }
+  for(int i = firstarg; i < argc; i++)
+  {
+    printf("%s", argv[i]);
+    if(i < argc - 1)
+    {
+      printf(" ");
+    }
+  }
+  if(!nline)
+  {
+    printf("\n");
+  }
+  return 0;
 }

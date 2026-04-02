44ac5e83fe1b22c4973dda5a3c3a742ad6c1526b
joaoamorimgamer <cachorro11_nando@proton.me>
2026-04-02 16:10:04 -0300

    dog now has EOF delimiter support

--- dog.c
+++ dog.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#define LINE 1024
+#define END "EOF\n" "_ACEOF\n" "<<EOF\n"
 void d_stream(FILE *ifp)
 {
 	int c;
@@ -11,25 +14,27 @@ void d_stream(FILE *ifp)
 }
 int main(int argc, char **argv)
 {
-    FILE *file;
-    float version = 1.1;
-    int year = 2026;
-    if(argc == 1 || strcmp(argv[1], "-") == 0)
+    char buffer[LINE];
+    if(argc == 1)
     {
 	    d_stream(stdin);
+      while(fgets(buffer, LINE, stdin) != NULL)
+      {
+        if(strcmp(buffer, END) == 0)
+        {
+          break;
+        }
+      }
     }
     else
     {
     	for (int i = 1; i < argc; i++)
     	{
-		file = fopen(argv[i], "r");
-		if(file == NULL)
+		FILE *file = fopen(argv[i], "r");
+		if (file == NULL)
 		{
-			if(strcmp(argv[1], "--version") != 0)
-			{
-				fprintf(stderr, "%s: %s\n", argv[0], strerror(errno));
-			}
-			continue;
+			fprintf(stderr, "%s: %s\n", argv[0], strerror(errno));
+			continue; 
 		}
 		else
 		{
@@ -37,16 +42,6 @@ int main(int argc, char **argv)
 			fclose(file);
        		}
      	}
-    }
-    if(strcmp(argv[1], "--version") == 0)
-    {
-	printf("%s (GAURUS) %.1lf\n", argv[0], version);
-	printf("Copyright (C) %d João Pedro Amorim Oliveira.\n", year);
-	printf("License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n");
-	printf("This is free software: you are free to change and redistribute it.\n");
-	printf("There is NO WARRANTY, to the extent permitted by law.\n\n");
-	printf("Written by João Pedro Amorim Oliveira.\n");
-	return 0;
     }
     return 0;
 }

ad6a2268dcf39f0578e2d24f2010f03cede4a1f3
joaoamorimgamer <cachorro11_nando@proton.me>
2026-04-07 20:41:58 -0300

    crdir with getopt, minor --help and --version changes

--- crdir.c
+++ crdir.c
@@ -1,39 +1,66 @@
 #include <stdio.h>
 #include <string.h>
+#include <getopt.h>
 #include <sys/stat.h>
 #include <errno.h>
-int main(int argc, char **argv)
+
+#define PROGRAM_NAME "crdir"
+
+#define AUTHORS "João Pedro Amorim Oliveira"
+
+void
+usage()
 {
-	float version = 1.1;
+  printf("Usage: %s [OPTION]... DIRECTORY...\n", PROGRAM_NAME);
+	printf("Create the DIRECTORIE(s), if they do not already exist.\n\n");
+	printf("  -h, --help		print this help page and exit\n");
+	printf("  -v, --version		show information about the version and exit\n");
+}
+
+int 
+main(int argc, char **argv)
+{
+  opterr = 0;
+  int c;
+	float version = 1.3;
 	int year = 2026;
+  static struct option const longopts[] =
+  {
+    {"help", no_argument, NULL, 'h'},
+    {"version", no_argument, NULL, 'v'},
+    {NULL, 0, NULL, 0}
+  };
+
 	if(argc == 1)
 	{
 		printf("%s: missing operand\n", argv[0]);
 		printf("Try '%s --help' for more information.\n", argv[0]);
 		return 1;
 	}
-	else if(argc > 1)
-	{
-		if(strcmp(argv[1], "--help") == 0)
-		{
-			printf("Usage: %s [OPTION]... DIRECTORY...\n", argv[0]);
-			printf("Create the DIRECTORIE(s), if they do not already exist.\n\n");
-			printf("  -p, --parents		creates child directories\n");
-			printf("  -v, --verbose		prints a message for every directory created\n");
-			printf("  --help		print this help page and exit\n");
-			printf("  --version		show information about the version and exit\n");
-			return 0;
-		}
-		else if(strcmp(argv[1], "--version") == 0)
-		{
-			printf("%s (GAURUS) %.1lf\n", argv[0], version);
-			printf("Copyright (C) %d João Pedro Amorim Oliveira.\n", year);
-			printf("License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n");
-			printf("This is free software: you are free to change and redistribute it.\n");
-			printf("There is NO WARRANTY, to the extent permitted by law.\n\n");
-			printf("Written by João Pedro Amorim Oliveira.\n");
-			return 0;
-		}
+  while((c = getopt_long(argc, argv, "hv", longopts, NULL)) != -1)
+  {
+    switch(c)
+    {
+      case 'h':
+        usage();
+        return 0;
+      case 'v':
+        printf("%s (GAURUS coreutils) %.1lf\n", argv[0], version);
+			  printf("Copyright (C) %d %s.\n", year, AUTHORS);
+			  printf("License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n");
+			  printf("This is free software: you are free to change and redistribute it.\n");
+			  printf("There is NO WARRANTY, to the extent permitted by law.\n\n");
+			  printf("Written by %s.\n", AUTHORS);
+			  return 0;
+      default:
+        for(int i = 1; i < argc; i++)
+        {
+          if(mkdir(argv[i], 0755) == 0)
+	        {
+	  	      return 0;
+	        }
+        }
+    }
 	}
   for(int i = 1; i < argc; i++)
   {
